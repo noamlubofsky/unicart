4f4d9f9505074ad7c7724fabee2519939b93184c
Noam Lubofsky <noamlubofsky@gmail.com>
2021-12-21 13:02:29 -0500

    switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router,
-  Routes,
-  Route,
-   } from "react-router-dom";
-// import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 import Products from "./components/Products";
